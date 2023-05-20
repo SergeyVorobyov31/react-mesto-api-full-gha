974b875358e165e9a84cc64cdd23ff62d2578b50
Sergey Vorobyov <cerega.vorobjev@yandex.ru>
2023-05-20 12:52:08 +0300

    fix: Удалить роут crash-test

--- backend/app.js
+++ backend/app.js
@@ -18,12 +18,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://127.0.0.1/mestodb').then(() => console.log('Успешное подключение к MongoDB')).catch((err) => console.error('Ошибка подключения:', err));
 
-app.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
-
 app.use(cors());
 app.use(requestLogger);
 
